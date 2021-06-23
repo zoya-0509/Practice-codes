3c70d4819fb476eca2243d98bc2c1ccfc2a50a85
zoya-0509 <85542292+zoya-0509@users.noreply.github.com>
2021-06-23 13:17:43 +0530

    Add files via upload

---
+++ Codechef GCD and LCM.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+long long  gcd(long long int a, long long int b)
+{
+if (b == 0)
+	return a;
+return gcd(b, a % b);
+}
+long long lcm(int a, int b)
+{
+	return (a / gcd(a, b)) * b;
+}
+int main()
+{
+      int t;
+      cin>>t;
+      while(t--)
+      {int a,b;
+	cin>>a>>b;
+	cout<<gcd(a,b)<<" "<<lcm(a, b)<<"\n";
+	}return 0;
+}
